6683fa0ee4673cd8dc38052b6eb78133fe7a177e
maayanch23-meet <maayanchorin@gmail.com>
2022-07-31 18:55:18 +0300

    finished lab

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -8,7 +8,7 @@ config = {  "apiKey": "AIzaSyAcbDxtC0jlPqyEH_GuUc0hzgPD7LTrAE8",
   "storageBucket": "fir-lab-81cd8.appspot.com",
   "messagingSenderId": "140815701353",
   "appId": "1:140815701353:web:cdf0f5c658c28ec4a019f8",
-  "measurementId": "G-4Y8H1V4R17"}
+  "measurementId": "G-4Y8H1V4R17", "databaseURL" : ""}
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
@@ -18,11 +18,30 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+         try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+         except:
+           error = "Authentication failed"
+    return render_template("signin.html")
+
+
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+           login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+           return redirect(url_for('add_tweet'))
+        except:
+           error = "Authentication failed"
     return render_template("signup.html")
 
 
